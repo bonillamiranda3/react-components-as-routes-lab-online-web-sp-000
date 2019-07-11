1a2e7bda2981e228d87a071ac8499c88f315d904
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-07-11 04:19:46 +0000

    Automatically backed up by Learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -5,6 +5,8 @@ const Directors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((director) => <div><h2>{director.name}</h2>{director.movies.map((movie) => <ul><li>{movie}</li></ul>)}</div>)}
     </div>
   );
 }
