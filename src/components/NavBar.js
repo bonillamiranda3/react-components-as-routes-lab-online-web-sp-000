09184bef0ef17af65ea0cf5dcba0bb657836a44e
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-07-11 04:29:04 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,12 +3,14 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
+    <Router>
     <div className="navbar">
     <NavLink to="/">Home</NavLink>
     <NavLink to="/directors">Directors</NavLink>
     <NavLink to="/movies">Movies</NavLink>
     <NavLink to="/actors">Actors</NavLink>
     </div>
+    </Router>
   );
 };
 
